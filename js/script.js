f6309062069f72d896a75ebbe0c2b6758f512304
Дмитрий Тракало <green_worker@rambler.ru>
2024-10-24 20:50:35 +0300

    add fetch data

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
-const url = "http://api.weatherapi.com/v1/forecast.json?key=10677ee246284bbbac4180434242110&q=Minsk&days=5&lang=ru"
+// current-card
+const valueHumidityKnot = document.querySelector(".current-indicators__humidity")
+const valueTempKnot = document.querySelector(".current-indicators__temp")
+const valueFeelingKnot = document.querySelector(".current-indicators__feeling")
+const valueWindSpeedKnot = document.querySelector(".card__wind-speed")
+const valueConditionKnot = document.querySelector(".card__condition")
+
+const urlAPI = "http://api.weatherapi.com/v1/forecast.json?key=10677ee246284bbbac4180434242110&q=Minsk&days=5&lang=en"
+
+function render(data) {
+    valueHumidityKnot.textContent = data.current.humidity + "%"
+    valueTempKnot.textContent = Math.round(data.current.temp_c) + "°C"
+    valueFeelingKnot.textContent = Math.round(data.current.feelslike_c) + "°C"
+    valueWindSpeedKnot.textContent = data.current.wind_kph + " km/h"
+    valueConditionKnot.textContent = data.current.condition.text
+}
 
 async function fetchData(url) {
     const response = await fetch(url)
     const data = await response.json()
     console.log(data)
+    render(data)
 }
 
-// fetchData(url)
+document.addEventListener("DOMContentLoaded", fetchData(urlAPI))
